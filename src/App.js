22146a199c47c2002f0a1e0bb083eb8896dcda37
Ravina-Rajmane <ravinarajmane95@gmail.com>
2023-09-22 17:24:52 +0530

    about and home page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Navbar from './Component/Navbar'
-
+import Home from './Component/Home'
+import About from './Component/About'
+import Menu from './Component/Menu'
+import Product from './Component/Product'
+import Review from './Component/Review'
+import Blog from './Component/Blog'
+import Contact from './Component/Contact'
+import Footer from './Component/Footer'
 const App = () => {
   return (
-    <div><Navbar></Navbar></div>
+    <div>
+      <Navbar></Navbar>
+      <Home></Home>
+      <About></About>
+      <Menu></Menu>
+      <Product></Product>
+      <Review></Review>
+      <Contact></Contact>
+      <Blog ></Blog>
+      <Footer></Footer>
+     
+    
+    </div>
   )
 }
 
---
+++ src/Component/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <section className='home' id='home'> 
+        <div className='content'>
+            <h3>
+               FRESH <span> FOOD IN THE </span>MORNING
+            </h3>
+            <p>
+                Lorem Issum dhf dfdjfkjfdf djsjfsjfjs sfjjsf.
+                dsfsdfs
+                Labore sint cupidiate distihhnfng Tbbdsdgs Rbdbsg.
+
+            </p>
+            <a href='#' className='btn'>
+                get yours now
+            </a>
+        </div>
+        
+        
+        </section>
+
+    </div>
+  )
+}
+
+export default Home
